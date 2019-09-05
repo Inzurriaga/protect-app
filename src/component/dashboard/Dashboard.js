6b443c88eb9373328223e68f162de48b2bcabc12
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2019-09-05 09:16:34 -0600

    setup dashboard

--- src/component/dashboard/Dashboard.js
+++ src/component/dashboard/Dashboard.js
@@ -1,10 +1,12 @@
 import React, {  Component ,Fragment } from "react";
+import { connect } from "react-redux";
 import {
   View,
-  Text
+  Text,
+  SafeAreaView
 } from "react-native";
 
-export default class DashBoard extends Component {
+export class DashBoard extends Component {
   constructor() {
     super();
     this.state = {
@@ -15,10 +17,37 @@ export default class DashBoard extends Component {
   render() {
     return(
       <Fragment>
-        <View>
-          <Text>hello im the bashboard</Text>
-        </View>
+        <SafeAreaView>
+          <View>
+            <Text>Heart Rate</Text>
+            <Text>93 bpm</Text>
+          </View>
+          <View>
+            <Text>SPO2</Text>
+            <Text>93 bpm</Text>
+          </View>
+          <View>
+            <Text>Temp</Text>
+            <Text>98 F</Text>
+          </View>
+          <View>
+            <View>
+              <Text>Co2</Text>
+              <Text>120 pp</Text>
+            </View>
+            <View>
+              <Text>CO</Text>
+              <Text>110 pp</Text>
+            </View>
+            <View>
+              <Text>H2S</Text>
+              <Text>120 pp</Text>
+            </View>
+          </View>
+        </SafeAreaView>
       </Fragment>
     ) 
   }
 }
+
+export default connect()(DashBoard)
